d586dfcc03dc49d91dbd6259e44783c2cdff8730
vladyn <vlado@vlado1.com>
2016-11-11 20:44:33 +0200

    Adding raoutes

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
   selector: 'fountain-app',
-  template: require('./main.html')
+  template: require('./main.html'),
+  directives: [ROUTER_DIRECTIVES]
 })
 export class MainComponent {}
--- src/app/some.js
+++ src/app/some.js
@@ -1,8 +1,10 @@
 import {Component} from "@angular/core";
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
   selector: 'some-selector',
-  template: require('./some.html')
+  template: require('./some.html'),
+  directives: [ROUTER_DIRECTIVES]
 })
 export class SomeComponent {
   constructor() {
