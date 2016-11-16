48cca2515fa33283c6c8c49704d9e2a5a2d577e7
vladyn <vlado@vlado1.com>
2016-11-17 00:38:30 +0200

    clearing an example component

--- src/app/body.js
+++ src/app/body.js
@@ -6,16 +6,16 @@ import {Http} from '@angular/http';
   templateUrl: './body.html'
 })
 export class BodyComponent implements OnInit {
-  texts: any[] = [];  // start with an empty array
-
-  constructor(http: Http) { }  // inject Http here, no need to assign to this
+  constructor(http: Http) {
+    this.texts = []; // start with an empty array
+  }  // inject Http here, no need to assign to this
 
   ngOnInit() {
     this.http
       .get('app/texts/main.json')
       .map(response => response.json())
       .subscribe(result => {
-        console.log(result);  // only log *inside* the callback
+        console.log(`result is ${result}`);  // only log *inside* the callback
         this.texts = result;
       });
     // outside the callback, the HTTP call hasn't yet finished
