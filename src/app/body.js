3352f3da6ed145af23816893992c1026096ff578
vladyn <vlado@vlado1.com>
2016-11-26 20:07:14 +0200

    a routes has been added

--- src/app/body.js
+++ src/app/body.js
@@ -1,21 +1,24 @@
 import {Component, OnInit} from '@angular/core';  // note consistent quotes
 import {Http} from '@angular/http';
+import * as route from '@angular/router';
 
 @Component({
   selector: 'body-selector',
-  templateUrl: './body.html'
+  template: require('./body.html')
 })
 export class BodyComponent implements OnInit {
-  constructor(http: Http) {
+  constructor(http: Http, route: route) {
     this.texts = []; // start with an empty array
+    this.http = http;
+    this.route = route;
   }  // inject Http here, no need to assign to this
 
   ngOnInit() {
+    console.table(this);
     this.http
       .get('app/texts/main.json')
       .map(response => response.json())
       .subscribe(result => {
-        console.log(`result is ${result}`);  // only log *inside* the callback
         this.texts = result;
       });
     // outside the callback, the HTTP call hasn't yet finished
