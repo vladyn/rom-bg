e61383d41acb431a222e258e9067f585b594aa2b
vladyn <vlado@vlado1.com>
2016-11-26 21:25:22 +0200

    adding proper router

--- src/app/body.js
+++ src/app/body.js
@@ -1,16 +1,16 @@
 import {Component, OnInit} from '@angular/core';  // note consistent quotes
 import {Http} from '@angular/http';
-import * as route from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'body-selector',
   template: require('./body.html')
 })
 export class BodyComponent implements OnInit {
-  constructor(http: Http, route: route) {
+  constructor(http:Http, route:Router) {
     this.texts = []; // start with an empty array
-    this.http = http;
     this.route = route;
+    this.http = http;
   }  // inject Http here, no need to assign to this
 
   ngOnInit() {
